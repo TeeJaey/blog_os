08a054ea50610efc4144360c43721139237ae693
TeeJaey <tjkaiser97@gmail.com>
2023-11-21 16:27:39 +0100

    replaced enum to dynamically register new IRQs

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,40 +1,68 @@
-// use crate::{gdt, hlt_loop, print, println};
-use crate::{gdt, hlt_loop, println};
+use crate::{gdt, hlt_loop, print, println};
+use alloc::{vec::Vec, string::String};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
-use spin;
+use spin::Mutex;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 pub const PIC_1_OFFSET: u8 = 0x20;
 pub const PIC_2_OFFSET: u8 = 0x28;
 
+static mut COUNT_DOWN: u32 = 0;
 
-// static PIC_1_COMMAND_PORT: Mutex<Port<u32>> = Mutex::new(Port::new(0x0020));
-// static PIC_1_DATA_PORT: Mutex<Port<u32>> = Mutex::new(Port::new(0x0021));
-// static PIC_2_COMMAND_PORT: Mutex<Port<u32>> = Mutex::new(Port::new(0x00A0));
-// static PIC_2_DATA_PORT: Mutex<Port<u32>> = Mutex::new(Port::new(0x00A1));
+// #[derive(Debug, Clone, Copy)]
+// #[repr(u8)]
+// pub enum InterruptIndex {
+//     Timer = PIC_1_OFFSET,
+//     Keyboard,
+//     RTL8139 = PIC_1_OFFSET + 11,
+// }
 
+// impl InterruptIndex {
+//     fn as_u8(self) -> u8 {
+//         self as u8
+//     }
 
-#[derive(Debug, Clone, Copy)]
-#[repr(u8)]
-pub enum InterruptIndex {
-    Timer = PIC_1_OFFSET,
-    Keyboard,
-    RTL8139 = PIC_2_OFFSET + 3,
+//     fn as_usize(self) -> usize {
+//         usize::from(self.as_u8())
+//     }
+// }
+
+#[derive(Debug, Clone)]
+struct MyInterruptIndex {
+    table: Vec<(String, u8)>
 }
 
-impl InterruptIndex {
-    fn as_u8(self) -> u8 {
-        self as u8
+impl MyInterruptIndex {
+    fn new() -> Self {
+        MyInterruptIndex { table: Vec::new() }
     }
 
-    fn as_usize(self) -> usize {
-        usize::from(self.as_u8())
+    fn insert(&mut self, key: &str, value: u8) {
+        self.table.push((String::from(key), value));
+    }
+
+    fn get(&self, key: &str) -> Option<u8> {
+        for pair in &self.table {
+            if pair.0 == key {
+                return Some(pair.1);
+            }
+        }
+        None
     }
 }
 
-pub static PICS: spin::Mutex<ChainedPics> =
-    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+pub static PICS: Mutex<ChainedPics> =
+    Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+lazy_static! {
+    static ref INDEX: Mutex<MyInterruptIndex> = Mutex::new({
+        let mut index = MyInterruptIndex::new();
+        index.insert("Timer", PIC_1_OFFSET);
+        index.insert("Keyboard", PIC_1_OFFSET + 1);
+        index
+    });
+}
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -46,15 +74,30 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
-        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
-        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
-        idt[InterruptIndex::RTL8139.as_usize()].set_handler_fn(rtl8139_interrupt_handler);
+        idt[INDEX.lock().get("Timer").unwrap() as usize].set_handler_fn(timer_interrupt_handler);
+        idt[INDEX.lock().get("Keyboard").unwrap() as usize].set_handler_fn(keyboard_interrupt_handler);
+        idt[INDEX.lock().get("RTL8139").unwrap() as usize].set_handler_fn(rtl8139_interrupt_handler);
         idt
     };
 }
 
 pub fn init_idt() {
     IDT.load();
+}
+
+pub unsafe fn sleep(time: u32) {
+    COUNT_DOWN = time;
+    println!("sleeping for {}", COUNT_DOWN);
+    while COUNT_DOWN > 0 {
+        print!(".");
+        COUNT_DOWN -= 1;
+        x86_64::instructions::hlt();
+    }
+    println!(".");
+}
+
+pub fn regiser_interrupt(name: &str, line: u8) {
+    INDEX.lock().insert(name, PIC_1_OFFSET + line)
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
@@ -85,7 +128,7 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
     // print!(".");
     unsafe {
         PICS.lock()
-            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+            .notify_end_of_interrupt(INDEX.lock().get("Timer").unwrap());
     }
 }
 
@@ -98,7 +141,7 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
 
     unsafe {
         PICS.lock()
-            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+            .notify_end_of_interrupt(INDEX.lock().get("Keyboard").unwrap());
     }
 }
 
@@ -108,7 +151,7 @@ extern "x86-interrupt" fn rtl8139_interrupt_handler(_stack_frame: InterruptStack
     
     unsafe {
         PICS.lock()
-            .notify_end_of_interrupt(InterruptIndex::RTL8139.as_u8());
+            .notify_end_of_interrupt(INDEX.lock().get("RTL8139").unwrap());
     }
 }
 
