8cfe3b7796f022388b36ba868c39879a5ad6b23f
TeeJaey <tjkaiser97@gmail.com>
2023-11-21 16:25:48 +0100

    created send.py to test receiving frames

---
+++ send.py
@@ -0,0 +1,12 @@
+from scapy.all import Ether, sendp
+
+# Ethernet frame details
+dst_mac = "ff:ff:ff:ff:ff:ff" # destination MAC address
+src_mac = "00:0e:c6:bd:96:2d" # source MAC address
+ether_type = 0x0800           # IP-type
+
+# Creating the Ethernet frame
+frame = Ether(dst=dst_mac, src=src_mac, type=ether_type)
+
+# Sending the Ethernet frame
+sendp(frame, iface="enx000ec6bd962d")
