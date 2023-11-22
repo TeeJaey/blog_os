5410dd559929e8d8353b9c6223d42cde5f49e41e
TeeJaey <tjkaiser97@gmail.com>
2023-11-22 18:50:26 +0100

    split ethernet frame in header and payload

--- src/ethernet.rs
+++ src/ethernet.rs
@@ -7,29 +7,43 @@ use core::mem::size_of_val;
 
 #[derive(Debug)]
 #[repr(C)]
-pub struct EthernetFrame {
+pub struct EthernetHeader {
     dst_mac: [u8; 6],
     src_mac: [u8; 6],
     protocol: u16,
-    payload: Vec<u8>
 }
 
-impl EthernetFrame {
+impl EthernetHeader {
     fn new(
         dst_mac: [u8; 6], 
         src_mac: [u8; 6], 
-        protocol: u16, 
+        protocol: u16
+    ) -> Self {
+        Self {dst_mac, src_mac, protocol}
+    }
+}
+
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct EthernetFrame {
+    header: EthernetHeader,
+    payload: Vec<u8>
+}
+impl EthernetFrame {
+    fn new(
+        header: EthernetHeader, 
         payload: Vec<u8>
     ) -> Self {
-        Self {dst_mac, src_mac, protocol, payload}
+        Self {header, payload}
     }
 
     fn to_bytes(&self) -> Vec<u8> {
         let mut result: Vec<u8> = Vec::new();
-        result.extend_from_slice(&self.dst_mac);
-        result.extend_from_slice(&self.src_mac);
-        let protocol_upper = ((self.protocol >> 8) & 0xff) as u8;
-        let protocol_lower = (self.protocol & 0xff) as u8;
+        result.extend_from_slice(&self.header.dst_mac);
+        result.extend_from_slice(&self.header.src_mac);
+        let protocol_upper = ((self.header.protocol >> 8) & 0xff) as u8;
+        let protocol_lower = (self.header.protocol & 0xff) as u8;
         result.push(protocol_upper);
         result.push(protocol_lower);
         result.extend(self.payload.iter());
@@ -40,22 +54,38 @@ impl EthernetFrame {
 
 fn send_frame(frame: EthernetFrame) {
 
-    let len = size_of_val(&frame);
+    let mut buffer: Vec<u8> = Vec::with_capacity(size_of_val(&frame));
 
-    let mut buffer: Vec<u8> = Vec::with_capacity(len);
     buffer.append(&mut frame.to_bytes());
+
     let buffer_virt_addr = VirtAddr::new(buffer.as_mut_ptr() as u64);
     
-    rtl8139::send_packet(buffer_virt_addr, len as u32)
+    rtl8139::send_packet(buffer_virt_addr, buffer.len() as u32)
 }
 
 pub fn send_empty_frame() {
-    let dst_mac: [u8; 6] = [0xff; 6];
-    let src_mac: [u8; 6] = rtl8139::get_mac_address();
-    let protocol = 0x0800; // IP-Ethernet-type
+    let header = EthernetHeader::new(
+        [0xff; 6],
+        rtl8139::get_mac_address(),
+        0x1122);
     let payload = Vec::new();
-
-    let empty_frame = EthernetFrame::new(dst_mac, src_mac, protocol, payload);
+    let empty_frame = EthernetFrame::new(header, payload);
 
     send_frame(empty_frame);
 }
+
+    // let payload: Vec<u8>  = Vec::from([
+        //     0x45,
+        //     0x00,
+        //     0x00, 0x00,
+        //     0x81, 0x39,
+        //     0x00, 0x00,
+        //     0x80,
+        //     0x11,
+        //     0x00, 0x00,
+        //     0x00, 0x00, 0x00 , 0x00,
+        //     0xff, 0xff, 0xff, 0xff,
+        //     0x22, 0x76,
+        //     0x22, 0x76,
+        //     0x00, 0x06
+        // ]);
