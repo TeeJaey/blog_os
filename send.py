b5181c81fd1d8c4d657ecbd4fa502de52ecdc137
TeeJaey <tjkaiser97@gmail.com>
2023-11-22 18:49:53 +0100

    small change in ethertype

--- send.py
+++ send.py
@@ -3,7 +3,7 @@ from scapy.all import Ether, sendp
 # Ethernet frame details
 dst_mac = "ff:ff:ff:ff:ff:ff" # destination MAC address
 src_mac = "00:0e:c6:bd:96:2d" # source MAC address
-ether_type = 0x0800           # IP-type
+ether_type = 0x1122          # EtherType
 
 # Creating the Ethernet frame
 frame = Ether(dst=dst_mac, src=src_mac, type=ether_type)
