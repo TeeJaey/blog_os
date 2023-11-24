98ded26a14f72da2ed217387a927c8f1b1a2d700
TeeJaey <tjkaiser97@gmail.com>
2023-11-24 17:10:05 +0100

    modified send.py to be dynamic

--- send.py
+++ send.py
@@ -1,12 +1,51 @@
 from scapy.all import Ether, sendp
+import netifaces
+
+def get_interfaces():
+    interfaces = []
+    print("Following Network Interfaces available:")
+    for i, iface in enumerate(netifaces.interfaces(), start=1):
+        print(f"{i}. {iface}")
+        interfaces.append(iface)
+    return interfaces
+
+def choose_interface(interfaces):
+    try:
+        choice = int(input("Please enter the number of the network interface you want to use: "))
+        if choice <= 0 or choice > len(interfaces):
+            print("Invalid number, please choose a number between 1 and", len(interfaces))
+            return choose_interface(interfaces)
+        else:
+            return interfaces[choice-1]
+    except ValueError:
+        print("Invalid input, please enter a number.")
+        return choose_interface(interfaces)
+    
+def get_mac_address(interface):
+    # get the mac address of the interface
+    mac_address = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]['addr']
+    return mac_address
+
+interfaces = get_interfaces()
+interface = choose_interface(interfaces)
+
+print("Chosen interface: ", interface)
 
 # Ethernet frame details
 dst_mac = "ff:ff:ff:ff:ff:ff" # destination MAC address
-src_mac = "00:0e:c6:bd:96:2d" # source MAC address
+src_mac = get_mac_address(interface) # source MAC address
 ether_type = 0x1122          # EtherType
 
-# Creating the Ethernet frame
-frame = Ether(dst=dst_mac, src=src_mac, type=ether_type)
+try:
+    num_frames = int(input("Please enter the number of frames you want to send: "))
+    if num_frames <= 0:
+        print("Invalid number, please enter a number greater than 0.")
+        num_frames = int(input("Please enter the number of frames you want to send: "))
+except ValueError:
+    print("Invalid input, please enter a number.")
+    num_frames = int(input("Please enter the number of frames you want to send: "))
 
-# Sending the Ethernet frame
-sendp(frame, iface="enx000ec6bd962d")
+# Creating that many frames and sending them at the end
+frames = [Ether(dst=dst_mac, src=src_mac, type=ether_type) for _ in range(num_frames)]
+print("Sending following frames: ", frames)
+sendp(frames, iface=interface)
