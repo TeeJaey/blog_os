8d540031016b1b0ce5fec710f117abf3ce23d2b6
TeeJaey <tjkaiser97@gmail.com>
2023-11-29 15:07:17 +0100

    repositioned blog_os::init() after memor::init()

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,14 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use x86_64::VirtAddr;
 
     println!("Hello World{}", "!");
-    blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    blog_os::init();
 
     #[cfg(test)]
     test_main();
